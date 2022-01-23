46a0965484e20a71dbd8d4b04d6e47ec2d2d1b67
berenice <berenice.tovar@kaufmann.cl>
2022-01-23 02:33:40 -0600

    Pagination and scroll into view functionalities have been added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import ResultList from "./components/ResultList";
 function App() {
   const [search, setSearch] = useState("");
   const [musicReleases, setMusicReleases] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     if (search === "") return;
@@ -15,24 +17,70 @@ function App() {
       const resultsPerPage = 18;
       const key = "zhCaEllGVyLwAekgqCSE";
       const token = "OHzlegNvJTDAdSsMuzVXDCTxhCzdOtao";
-      const url = `https://api.discogs.com/database/search?q=${search}&key=${key}&secret=${token}&per_page=${resultsPerPage}`;
+      const url = `https://api.discogs.com/database/search?q=${search}&key=${key}&secret=${token}&per_page=${resultsPerPage}&page=${currentPage}`;
 
       const response = await fetch(url);
       const result = await response.json();
 
       setMusicReleases(result.results);
-      console.log(result.results);
+
+      const numberOfPages = result.pagination.items;
+
+      const totalPages = Math.ceil(numberOfPages / resultsPerPage);
+      setTotalPages(totalPages);
+
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron!.scrollIntoView({ behavior: "smooth" });
     };
 
     callApi();
-  }, [search]);
+  }, [search, currentPage]);
+
+  //Pagination function - last page
+  const lastPage = () => {
+    const newCurrentPage = currentPage - 1;
+
+    if (newCurrentPage === 0) return;
+
+    setCurrentPage(newCurrentPage);
+  };
+
+  //Pagination function - next page
+  const nextPage = () => {
+    const newCurrentPage = currentPage + 1;
+
+    if (newCurrentPage === totalPages) return;
+
+    setCurrentPage(newCurrentPage);
+  };
+
   return (
     <div>
-      <div className="grid justify-items-center bg-zinc-700 pb-1">
+      <div className="grid justify-items-center bg-zinc-700 pb-1 jumbotron">
         <Form setSearch={setSearch} />
       </div>
-      <div>
+      <div className="justify-items-center">
         <ResultList musicReleases={musicReleases} />
+      </div>
+      <div className="flex justify-center">
+        {currentPage === 1 ? null : (
+          <button
+            type="button"
+            className="bg-blue-500 text-zinc-100 p-2 mt-7 mb-7"
+            onClick={lastPage}
+          >
+            &laquo; Prev
+          </button>
+        )}
+        {currentPage === totalPages ? null : (
+          <button
+            type="button"
+            className="bg-blue-500 text-zinc-100 p-2 ml-3 mt-7 mb-7"
+            onClick={nextPage}
+          >
+            Next &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
