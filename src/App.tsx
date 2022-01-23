b1ef50a9791678d8a952012f700e0e705db562e9
berenice <berenice.tovar@kaufmann.cl>
2022-01-22 22:44:30 -0600

    Design has been added to navbar plus api has been called

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 
-function App(): JSX.Element {
+function App() {
+  const [search, setSearch] = useState("");
+  //búsqueda - guardar búsqueda
+
+  useEffect(() => {
+    if (search === "") return;
+
+    const callApi = async () => {
+      const resultsPerPage = 20;
+      const key = "zhCaEllGVyLwAekgqCSE";
+      const token = "OHzlegNvJTDAdSsMuzVXDCTxhCzdOtao";
+      const url = `https://api.discogs.com/database/search?q=${search}&key=${key}&secret=${token}&per_page=${resultsPerPage}`;
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      setSearch(result.results);
+    };
+
+    callApi();
+  }, [search]);
   return (
     <div>
-      <div className="grid justify-items-center">
-        <h1 className="p-6">Discogs</h1>
-        <Form />
+      <div className="grid justify-items-center bg-zinc-700 pb-1">
+        <Form setSearch={setSearch} />
       </div>
     </div>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,34 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = ({ setSearch }: { setSearch: any }) => {
+  const [searchedText, setSearchedText] = useState("");
+  //termino - guardar termino
+
+  const searchInformation = (e: any) => {
+    e.preventDefault();
+
+    setSearch(searchedText);
+  };
+
   return (
-    <form>
-      <div className="columns-2">
-        <div className="">
+    <form onSubmit={searchInformation}>
+      <div className="flex flex-row">
+        <div className="basis-1/4">
+          <h1 className="p-4 text-2xl text-white mr-4">Discogs</h1>
+        </div>
+        <div className="basis-2/4">
           <input
             type="text"
             placeholder="Search the artist, example: Vicente Fernandez"
-            className="w-96 p-1"
+            className="w-96 p-1 mt-4"
+            onChange={(e: any) => setSearchedText(e.target.value)}
           />
         </div>
-        <div className="">
+        <div className="basis-1/4">
           <input
             type="submit"
-            placeholder="Search"
-            className="w-48 bg-red-400 text-zinc-100 rounded-md p-1"
+            value="Search"
+            className="w-32 bg-blue-500 text-zinc-100 rounded p-1 ml-3 mt-4"
           />
         </div>
       </div>
