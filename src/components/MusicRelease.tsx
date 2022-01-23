eb368a2d8f1313c69b2be11d9873bbbd42fa4a02
berenice <berenice.tovar@kaufmann.cl>
2022-01-23 01:08:46 -0600

    Link to redirect more info has been added

--- src/components/MusicRelease.tsx
+++ src/components/MusicRelease.tsx
@@ -1,10 +1,14 @@
 const MusicRelease = ({ musicRelease }: { musicRelease: any }) => {
-  const { thumb, title } = musicRelease;
+  const { thumb, title, uri } = musicRelease;
+
+  const url = "https://www.discogs.com" + uri;
   return (
     <div className="border-solid border-2 m-4 items-center justify-center">
       <div className="bg-white">
-        <img src={thumb} alt={title} />
-        <p className="text-sky-700">{title}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <img src={thumb} alt={title} />
+          <p className="text-sky-700">{title}</p>
+        </a>
       </div>
     </div>
   );
