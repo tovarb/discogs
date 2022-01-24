0a1c8949bf126eedbcb8b4b5a6c75b3be585ef5b
berenice <berenice.tovar@kaufmann.cl>
2022-01-23 21:44:16 -0600

    Styles for responsive view have been updated

--- src/components/MusicRelease.tsx
+++ src/components/MusicRelease.tsx
@@ -7,7 +7,7 @@ const MusicRelease = ({ musicRelease }: { musicRelease: musicReleaseInfo }) => {
 
   return (
     <div className="border-solid border-2 m-4 items-center justify-center">
-      <div className="bg-white pl-3 pt-3">
+      <div className="bg-white pl-2 pr-2 pt-3 pb-2 md:pl-3 md:pr-3 md:pt-3 lg:pl-3  lg:pr-3 lg:pt-4 lg:pb-4">
         <a href={url} target="_blank" rel="noopener noreferrer">
           <img src={thumb} alt={title} />
           <p className="text-sky-700">{title}</p>
--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -3,7 +3,7 @@ import { musicReleaseInfo } from "./musicReleaseInfo";
 
 const ResultList = ({ musicReleases }: { musicReleases: any }) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 ml-12 mr-12 ">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 md:ml-2 md:mr2 lg:ml-8 lg:mr-8 ">
       {musicReleases.map((musicRelease: musicReleaseInfo) => (
         <MusicRelease key={musicRelease.id} musicRelease={musicRelease} />
       ))}
